29a7f67f24aac787a1157ac26d61a09e485358ce
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2014-06-13 19:04:20 +0900

    change the way to install nosetests

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -21,14 +21,14 @@ $APT_GET_INSTALL redis-server
 # git
 $APT_GET_INSTALL git
 # python3
-$APT_GET_INSTALL python3 python3-dev python3-numpy python3-scipy python3-nose
+$APT_GET_INSTALL python3 python3-dev python3-numpy python3-scipy
 #python-numpy python-scipy python-django python-mysqldb python-nose python-matplotlib python-pip python-sklearn
 # pip update
 $APT_GET_INSTALL curl libfreetype6 libfreetype6-dev libatlas-dev libatlas-base-dev build-essential
 curl -kL https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python3
 __PIP_PACKAGES="
 pip pip-tools versiontools Django==1.6.5 freetype2
-matplotlib scikit-learn==0.14.1
+nose matplotlib scikit-learn==0.14.1
 pymysql django-mysql-pymysql redis django-redis
 "
 for __PACKAGE in $__PIP_PACKAGES
