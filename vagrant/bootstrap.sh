bdb83125224eead99aa31229c63d21c512a6011a
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2014-06-13 16:40:20 +0900

    modify vagrant/bootstrap.sh about pip

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,17 +6,24 @@ export DOCKER_HOST=localhost
 
 ## apt
 APT_GET_INSTALL='apt-get install -y --force-yes'
-apt-get update
+apt-get update -y
+apt-get upgrade -y
 # apache
 $APT_GET_INSTALL apache2 
 # mysql
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
 $APT_GET_INSTALL mysql-server
+## sqlite
+$APT_GET_INSTALL sqlite sqlite3
 # git
 $APT_GET_INSTALL git
 # python
 $APT_GET_INSTALL python python-numpy python-scipy python-django python-mysqldb python-nose python-matplotlib python-pip python-sklearn
+# pip update
+pip install -U pip-tools
+pip-review --auto
+#pip freeze --local | cut -d = -f 1  | while read i; do pip install -U $i; done
 # misc
 $APT_GET_INSTALL screen bash-completion
 
