a1e9cc161ee38f71393f9dea5b4788db0d4dc373
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2014-06-17 14:55:16 +0900

    add tests of get_client_ip() and get_user_agent()

--- webapp/webapp/tests/views/test_pages.py
+++ webapp/webapp/tests/views/test_pages.py
@@ -11,11 +11,6 @@ class TestPagesView(TestCase):
         self.client  = Client()
         self.factory = RequestFactory()
 
-    def test_generate_user_id_hash(self):
-        request = self.factory.get("/", HTTP_USER_AGENT='Mozilla/5.0')
-        result = generate_user_id_hash(request)
-        self.assertEqual(result, '6328c09c3f9c4817b14fddbb73d9602e1c09562e')
-
     def test_page_a(self):
         request = self.factory.get("/page-a/", HTTP_USER_AGENT='Mozilla/5.0')
         response = page_a(request)
@@ -31,3 +26,17 @@ class TestPagesView(TestCase):
         response = page_a(request)
         self.assertEqual(response.status_code, 200)
         
+    def test_generate_user_id_hash(self):
+        request = self.factory.get("/", HTTP_USER_AGENT='Mozilla/5.0')
+        result = generate_user_id_hash(request)
+        self.assertEqual(result, '6328c09c3f9c4817b14fddbb73d9602e1c09562e')
+
+    def test_get_client_ip(self):
+        request = self.factory.get("/", HTTP_USER_AGENT = 'Mozilla/5.0')
+        result  = get_client_ip(request)
+        self.assertEqual(result, '127.0.0.1')
+
+    def test_get_user_agent(self):
+        request = self.factory.get("/", HTTP_USER_AGENT = 'Mozilla/5.0')
+        result = get_user_agent(request)
+        self.assertEqual(result, 'Mozilla/5.0')
