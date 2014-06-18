d535b55aa7705853e7b56f94baf1576daeb13594
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2014-06-18 13:49:22 +0900

    fix sbin/runner.sh

--- webapp/sbin/runserver.sh
+++ webapp/sbin/runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export __CURRENT_DIR=`basename $0`
-pushd "$__CURRENT_DIR"
+export __CURRENT_DIR=`dirname $0`
+cd "${__CURRENT_DIR}/.."
 
 export __PYTHON='python3'
 export __IP_ADDRESS='192.168.111.228'
